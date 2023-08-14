97587c3535a708367826637d511b9c2c4064a901
euewrqe <euewrqe@gmail.com>
2023-08-14 14:00:36 +0900

    vue-1-1

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,17 @@
   <Greet name="Bruce" />
   <Greet name="Clark" />
   <Greet name="Diana" />
+  <button class="btn btn-primary">button</button>
+  <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Dropdown button
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+  </ul>
+</div>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
+// import "../node_modules/bootstrap/dist/css/bootstrap.css"
 import { createApp } from 'vue'
 import App from './App.vue'
+import "./assets/style/main.scss"
 
 createApp(App).mount('#app')
+
+import "../node_modules/bootstrap/dist/js/bootstrap"
